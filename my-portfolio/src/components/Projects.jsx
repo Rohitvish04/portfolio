e44dff6b2d6d8b5189b37973bcfe1e9635087eb6
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2025-08-29 21:54:13 +0530

    Update Projects.jsx

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -26,7 +26,7 @@ const projects = [
     title: 'Portfolio Website',
     description: 'Modern personal portfolio showcasing skills & projects.',
     tech: ['React', 'Material UI', 'Vite'],
-    image: '/projects/portfolio.jpg',
+    image: 'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     github: 'https://github.com/yourusername/portfolio',
     live: 'https://blogs-api-ncn3.onrender.com',
   },
