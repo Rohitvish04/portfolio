2e1c47ef33fda1e126bd42200e26ba892dab9348
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2025-09-26 21:15:34 +0530

    Update Projects.jsx

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -17,7 +17,7 @@ const projects = [
   {
     title: 'Blog Website',
     description: 'Responsive blog site with user login, create & edit posts.',
-    tech: ['React', 'Node.js', 'MongoDB'],
+    tech: ['React', 'Node.js', 'NeonDB', 'Prisma', 'ORM'],
     image: 'https://plus.unsplash.com/premium_photo-1720744786849-a7412d24ffbf?q=80&w=3418&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     github: 'https://github.com/Rohitvish04/Blogs_API',
     live: 'https://blogs-api-ncn3.onrender.com',
